0b8aff160c8b4fb3579608ea6149fa16ac985806
Harmander Sihra <sihraharmander@gmail.com>
2021-12-06 15:02:46 -0800

    Added python script which does the replacing

---
+++ script.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# This script checks for the file "BuildVersion.h" and replaces it with the
+# correct semver numbers from the git history (these are inputs)
+
+import fileinput
+import sys
+
+def main(major, minor, patch):
+    buildVersionFileName = 'BuildVersion.h'
+
+    for line in fileinput.input(buildVersionFileName, inplace = 1):
+        if "major" in line:
+            print(line.replace("0", major).rstrip())
+        elif "minor" in line:
+            print(line.replace("0", minor).rstrip())
+        elif "patch" in line:
+            print(line.replace("0", patch).rstrip())
+        else:
+            print(line.rstrip())
+
+
+if __name__ == '__main__':
+    # Get major, minor, and patch versions from inputs
+    if len(sys.argv) != 4:
+        sys.exit("Correct usage: script.py <major version> <minor number> <patch number>")
+
+    major = sys.argv[1]
+    minor = sys.argv[2]
+    patch = sys.argv[3]
+
+    main(major, minor, patch)
