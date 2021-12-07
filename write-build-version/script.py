f8131c1cad649a0d71c6115c6179341302b9d977
Harmander Sihra <harmander.sihra@staflsystems.com>
2021-12-07 15:33:06 -0800

    This action now populates the build version field in BuildVersion.h as well
    * Update action.yml
    
    * Update script.py

--- write-build-version/script.py
+++ write-build-version/script.py
@@ -6,7 +6,7 @@
 import fileinput
 import sys
 
-def main(major, minor, patch):
+def main(major, minor, patch, build):
     buildVersionFileName = 'BuildVersion.h'
 
     for line in fileinput.input(buildVersionFileName, inplace = 1):
@@ -16,17 +16,20 @@ def main(major, minor, patch):
             print(line.replace("0", minor).rstrip())
         elif "patch" in line:
             print(line.replace("0", patch).rstrip())
+        elif "build" in line:
+            print(line.replace("0", build).rstrip())
         else:
             print(line.rstrip())
 
 
 if __name__ == '__main__':
     # Get major, minor, and patch versions from inputs
-    if len(sys.argv) != 4:
+    if len(sys.argv) != 5:
         sys.exit("Correct usage: script.py <major version> <minor number> <patch number>")
 
     major = sys.argv[1]
     minor = sys.argv[2]
     patch = sys.argv[3]
+    build = sys.argv[4]
 
-    main(major, minor, patch)
+    main(major, minor, patch, build)
