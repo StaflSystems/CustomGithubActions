2d90f8190bcd4bdd620d9d8c67648d91229e4c13
Harmander Sihra <sihraharmander@gmail.com>
2021-12-08 10:29:56 -0800

    Updated python build script

--- CCStudioBuild/script.py
+++ CCStudioBuild/script.py
@@ -6,20 +6,37 @@ import subprocess
 import sys
 
 eclipse_path = 'C:/ti/ccs1100/ccs/eclipse/eclipsec.exe'
-project_name = str(sys.argv[1])
-os.chdir("..")
-
-workspace = os.getcwd()
 
 
-# Import the main project
-import_command = "{} -noSplash -data {} -application com.ti.ccstudio.apps.projectImport -ccs.location {}\{} -ccs.overwrite".format(eclipse_path, workspace, workspace, project_name)
-os.system(import_command)
+def main(project_name, build_config):
+    os.chdir("..")
+    workspace = os.getcwd()
 
-# Import stafllib
-submodule_import_command = "{} -noSplash -data {} -application com.ti.ccstudio.apps.projectImport -ccs.location {}\{}\stafllib -ccs.overwrite".format(eclipse_path, workspace, workspace, project_name)
-os.system(submodule_import_command)
+    # Import the main project
+    import_command = "{} -noSplash -data {} -application com.ti.ccstudio.apps.projectImport -ccs.location " \
+                     "{}\{} -ccs.overwrite".format(eclipse_path, workspace, workspace, project_name)
+    if os.system(import_command) != 0:
+        raise RuntimeError
 
-# Build project
-build_command = "{} -noSplash -data {} -application com.ti.ccstudio.apps.projectBuild -ccs.projects {} -ccs.configuration Debug-Stafl".format(eclipse_path, workspace, project_name)
-os.system(build_command)
+    # Import stafllib
+    submodule_import_command = "{} -noSplash -data {} -application com.ti.ccstudio.apps.projectImport -ccs.location " \
+                               "{}\{}\stafllib -ccs.overwrite".format(eclipse_path, workspace, workspace, project_name)
+    if os.system(submodule_import_command) != 0:
+        raise RuntimeError
+
+    # Build project
+    build_command = "{} -noSplash -data {} -application com.ti.ccstudio.apps.projectBuild -ccs.projects {} " \
+                    "-ccs.configuration {}".format(eclipse_path, workspace, project_name, build_config)
+    if os.system(build_command) != 0:
+        raise RuntimeError
+
+
+if __name__ == '__main__':
+    # We first make sure we have the right amount of arguments
+    if len(sys.argv) != 3:
+        sys.exit("Invalid usage. Correct usage: ./script.py <project_name> <build_config>")
+
+    project_name = sys.argv[1]
+    build_config = sys.argv[2]
+
+    main(project_name, build_config)
