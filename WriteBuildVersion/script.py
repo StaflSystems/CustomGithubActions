f6a54506c84ea36b40b6e69c9fee9dd706242025
Marco Nunez <m.nunez2105@gmail.com>
2023-01-19 01:03:39 +0000

    use pre release indices instead of build meta data

--- WriteBuildVersion/script.py
+++ WriteBuildVersion/script.py
@@ -7,7 +7,13 @@ import fileinput
 import sys
 import os
 
-def main(filepath, major, minor, patch, build):
+def main(filepath, major, minor, patch, pr_tag, pr_number):
+    # Figure out which pre-release index to use
+    if(pr_tag.isnumeric()):
+        build = pr_tag
+    else:
+        build = pr_number
+
     # We first check to see if the file exists
     if (os.path.isfile(filepath)):
         print("Updating BuildVersion.h")
@@ -30,13 +36,14 @@ def main(filepath, major, minor, patch, build):
 
 if __name__ == '__main__':
     # Get major, minor, and patch versions from inputs
-    if len(sys.argv) < 5:
-        sys.exit("Correct usage: script.py <file_path> <major version> <minor number> <patch number> <build number>")
+    if len(sys.argv) < 6:
+        sys.exit("Correct usage: script.py <file_path> <major version> <minor number> <patch number> <PreRelease Tag> <PreRelease Number>")
 
     filepath = sys.argv[1]
     major = sys.argv[2]
     minor = sys.argv[3]
     patch = sys.argv[4]
-    build = sys.argv[5] if len(sys.argv) == 6 else '0'
+    pr_tag = sys.argv[5]
+    pr_number = sys.argv[6]
 
-    main(filepath, major, minor, patch, build)
+    main(filepath, major, minor, patch, pr_tag, pr_number)
