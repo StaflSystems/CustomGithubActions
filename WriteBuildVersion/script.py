0e516adfe217c6a1c56aedc5e441daa8c455e1d4
Thomas Willson <thomas.willson@staflsystems.com>
2023-01-25 10:08:00 -0800

    Revert "Use PreRelease GitVersion Indices (#28)"
    This reverts commit 436154d8cc4b06d6137fdfd439a3dac7da56eaf4.

--- WriteBuildVersion/script.py
+++ WriteBuildVersion/script.py
@@ -7,18 +7,10 @@ import fileinput
 import sys
 import os
 
-def main(filepath, major, minor, patch, pre_release_tag, pre_release_number):
-    # Figure out which pre-release index to use
-    if(not pre_release_tag.isnumeric() and not pre_release_number.isnumeric()):
-        build = '0'
-    elif(pre_release_tag.isnumeric()):
-        build = pre_release_tag
-    else:
-        build = pre_release_number
-
+def main(filepath, major, minor, patch, build):
     # We first check to see if the file exists
     if (os.path.isfile(filepath)):
-        print("Updating BuildVersion.h with "+major+"."+minor+"."+patch+"+"+build)
+        print("Updating BuildVersion.h")
 
         for line in fileinput.input(filepath, inplace = 1):
             if "major" in line:
@@ -39,21 +31,12 @@ def main(filepath, major, minor, patch, pre_release_tag, pre_release_number):
 if __name__ == '__main__':
     # Get major, minor, and patch versions from inputs
     if len(sys.argv) < 5:
-        sys.exit("Correct usage needs at least: script.py <file_path> <major version> <minor number> <patch number>")
+        sys.exit("Correct usage: script.py <file_path> <major version> <minor number> <patch number> <build number>")
 
     filepath = sys.argv[1]
     major = sys.argv[2]
     minor = sys.argv[3]
     patch = sys.argv[4]
-    
-    try:
-        pre_release_tag = sys.argv[5]
-    except:
-        pre_release_tag = ''
-        
-    try:
-        pre_release_number = sys.argv[6]
-    except:
-        pre_release_number = ''
+    build = sys.argv[5] if len(sys.argv) == 6 else '0'
 
-    main(filepath, major, minor, patch, pre_release_tag, pre_release_number)
+    main(filepath, major, minor, patch, build)
