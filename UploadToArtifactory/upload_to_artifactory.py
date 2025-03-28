87e5f85796764cc0d4311813d463724def6a212c
Thomas Willson <thomas.willson@staflsystems.com>
2025-03-28 13:55:59 -0700

    Add upload to artifactory action.

---
+++ UploadToArtifactory/upload_to_artifactory.py
@@ -0,0 +1,30 @@
+import argparse
+
+from pathlib import Path
+from os import system
+
+
+def upload_to_artifactory(src_path: Path, repository: str, org: str, version: str):
+    print(f"Uploading {src_path} to {repository} with version {version}")
+
+    for file in src_path.iterdir():
+        if not file.is_file():
+            continue
+
+        sanitized_version = version.replace("/", "_")
+        module = file.stem.replace(f"-{sanitized_version}", "")
+
+        dest = f"{repository}/{org}/{module}/{file.name}"
+        print(f"Uploading {file} to {dest}")
+        system(f'jf rt u "{file}" "{dest}"')
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Upload to Artifactory")
+    parser.add_argument("repository", type=str, help="Repository")
+    parser.add_argument("org", type=str, help="Organization")
+    parser.add_argument("version", type=str, help="Version")
+    parser.add_argument("src_path", type=Path, help="Source path")
+    args = parser.parse_args()
+
+    upload_to_artifactory(args.src_path, args.repository, args.org, args.version)
