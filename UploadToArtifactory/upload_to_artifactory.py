a780918b68c4f0b7eb804bdbd9fa8be590dacf10
Thomas Willson <thomas.willson@staflsystems.com>
2025-03-31 17:52:56 -0700

    Support a globbing pattern for the Artifactory upload.

--- UploadToArtifactory/upload_to_artifactory.py
+++ UploadToArtifactory/upload_to_artifactory.py
@@ -4,10 +4,10 @@ from pathlib import Path
 from os import system
 
 
-def upload_to_artifactory(src_path: Path, repository: str, org: str, version: str):
+def upload_to_artifactory(src_path: Path, repository: str, org: str, version: str, pattern = "*"):
     print(f"Uploading {src_path} to {repository} with version {version}")
 
-    for file in src_path.iterdir():
+    for file in src_path.glob(pattern):
         if not file.is_file():
             continue
 
@@ -25,6 +25,7 @@ if __name__ == "__main__":
     parser.add_argument("org", type=str, help="Organization")
     parser.add_argument("version", type=str, help="Version")
     parser.add_argument("src_path", type=Path, help="Source path")
+    parser.add_argument("pattern", type=str, help="Pattern to match files")
     args = parser.parse_args()
 
-    upload_to_artifactory(args.src_path, args.repository, args.org, args.version)
+    upload_to_artifactory(args.src_path, args.repository, args.org, args.version, args.pattern)
